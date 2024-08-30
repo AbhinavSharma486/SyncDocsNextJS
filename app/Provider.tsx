4672858ffe0f79ddda8cc0ebd6bc689a337d921b
AbhinavSharma486 <abhinavparashar486@gmail.com>
2024-08-31 00:33:43 +0530

    User Mention Feature implemented

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import { ClientSideSuspense, LiveblocksProvider } from "@liveblocks/react/suspense";
 import { ReactNode } from "react";
 
 
 const Provider = ({ children }: { children: ReactNode; }) => {
+
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
@@ -14,6 +18,16 @@ const Provider = ({ children }: { children: ReactNode; }) => {
         const users = await getClerkUsers({ userIds });
 
         return users;
+      }}
+
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>
